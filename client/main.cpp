d525d9317848ec514d88c4a415b42d4d995a47fb
JoWatersASC <joshua.waters365@gmail.com>
2024-12-12 18:57:37 -0500

    Specified operating systems and how to find rtaudio in cmakelists.txt

--- client/main.cpp
+++ client/main.cpp
@@ -3,20 +3,29 @@
 using namespace osf;
 
 int main(int argc, char* argv[]) {
+#ifdef WIN32
+if (winsock_initialize())
+    return 1;
+#endif
+
     int cl_fd = socket(AF_INET, SOCK_STREAM, 0);
     sockaddr_in serv_addr;
 
     if(argc == 3)
         serv_addr = create_address(argv[1], std::stoi(argv[2]));
     else 
-        serv_addr = create_address("10.15.51.101", 4000);
+        serv_addr = create_address("10.15.51.101", 8000);
 
 
-    Client firstc(cl_fd, serv_addr);
+    Client firstc(cl_fd, serv_addr, 6);
     firstc.start_connect();
 
     firstc.start_recv();
     firstc.start_send();
 
     firstc.close();
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
 }
