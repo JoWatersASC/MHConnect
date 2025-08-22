d12ace9de7935b2bc177762e0a675758669a9324
Brouks <joshua.waters365@gmail.com>
2025-08-22 14:48:19 -0400

    Formatting standardization of tabs instead of spaces

--- server/include/server.h
+++ server/include/server.h
@@ -4,25 +4,24 @@
 
 namespace osf
 {
-    
+
 class Server {
 public:
-    Server(size_t _num_threads = 5) : recv_tp(_num_threads * 2 / 3), send_tp(_num_threads - (_num_threads * 2 / 3)),
-    m_room(recv_tp, send_tp), running(false)
-    {}
-    Server(const Server&) = delete;
+	Server(size_t _num_threads = 5) : recv_tp(_num_threads * 2 / 3), send_tp(_num_threads - (_num_threads * 2 / 3)),
+	m_room(recv_tp, send_tp), running(false)
+	{}
+	Server(const Server&) = delete;
 
-    void start();
-    void accept_client();
+	void start();
+	void accept_client();
 
 private:
-    socket_t m_sock_fd;
-    ThreadPool recv_tp;
-    ThreadPool send_tp;
-    
-    ChatRoom m_room;
-    std::atomic<bool> running;
+	socket_t m_sock_fd;
+	ThreadPool recv_tp;
+	ThreadPool send_tp;
 
+	ChatRoom m_room;
+	std::atomic<bool> running;
 };
 
-}
+} // namespace osf
--- server/src/chat_room.cpp
+++ server/src/chat_room.cpp
@@ -3,13 +3,13 @@
 using namespace osf;
 
 void ChatRoom::broadcast(const packet p, const id_t& source_id) {
-    std::unique_lock<std::mutex> lock(m_mtx);
+	std::unique_lock<std::mutex> lock(m_mtx);
 
 	if(p.type == PCKTYPE::TEXT) {
 		std::cout << "FROM: " << source_id << std::endl;
 		std::cout << "[MSG] " << p << std::endl;
 	}
-    
+
 	if(p.type == PCKTYPE::TEXT) {
 		for(Connection& _client : m_clients) {
 			if(!(_client.getId() == source_id)) {
@@ -17,6 +17,7 @@ void ChatRoom::broadcast(const packet p, const id_t& source_id) {
 			}
 		}
 	}
+
 	if(p.type == PCKTYPE::AUDIO) {
 		for(Connection& _client : m_clients) {
 			if(!(_client.getId() == source_id)) {
@@ -24,32 +25,30 @@ void ChatRoom::broadcast(const packet p, const id_t& source_id) {
 			}
 		}
 	}
-    
 }
 
 void ChatRoom::add_client(socket_t _fd, sockaddr_in _addr) {
-    std::unique_lock<std::mutex> lock(m_mtx);
+	std::unique_lock<std::mutex> lock(m_mtx);
 
-    id_t new_client_id;
+	id_t new_client_id;
 
-    do {
-        new_client_id = rand();
-    } while(client_ids.count(new_client_id));
-    
-    m_clients.emplace_back(recv_tp, send_tp, *this, 
-        std::move(_fd), _addr, new_client_id);
+	do {
+		new_client_id = rand();
+	} while(client_ids.count(new_client_id));
 
-    std::cout << "New client connected: ip-" << inet_ntoa(_addr.sin_addr) << std::endl;
+	m_clients.emplace_back(recv_tp, send_tp, *this, _fd, _addr, new_client_id);
 
-    client_ids.emplace(new_client_id, m_clients.back());
+	std::cout << "New client connected: ip-" << inet_ntoa(_addr.sin_addr) << std::endl;
 
-    packet id_pckt;
-    id_pckt.type = PCKTYPE::TEXT;
-    id_pckt.id = new_client_id;
-    id_pckt = std::string("ID assigned: " + std::to_string(new_client_id));
+	client_ids.emplace(new_client_id, m_clients.back());
 
-    m_clients.back().send_text(id_pckt);
-    recv_tp.add([&]() {
-        m_clients.back().recv_text(); 
-    });
+	packet id_pckt;
+	id_pckt.type = PCKTYPE::TEXT;
+	id_pckt.id = new_client_id;
+	id_pckt = std::string("ID assigned: " + std::to_string(new_client_id));
+
+	m_clients.back().send_text(id_pckt);
+	recv_tp.add([&]() {
+		m_clients.back().recv_text();
+	});
 }
